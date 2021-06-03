89a657baa980a57d8b4b687dde448c19901c8b95
ASTRO063 <manikanta.kodimala3@gmail.com>
2021-06-03 11:37:42 +0530

    multiplication-feature is added

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def divisionByTwo(additionValue = 6):
     return additionValue//2
+def multiplication(divisionvalue=2,h=3):
+    return divisionvalue*h
 
 if __name__=="__main__":
-    print(divisionByTwo())
+    divisionValue=divisionByTwo()
+    print(multiplication(divisionValue,3))
